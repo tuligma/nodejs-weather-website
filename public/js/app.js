304ef0f7172b7c188c7dad75e0027e55f74a6fac
tuligma <ricardopulupotjr@gmail.com>
2024-03-24 03:11:49 +0400

    Set up app for Vercel

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
 	result.textContent = 'Searching...'
 	result2.textContent = ''
 
-	fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+	fetch('/weather?address=' + location).then((response) => {
 		response.json().then((data) => {
 			search.value = ''
 			if (data.error) {
